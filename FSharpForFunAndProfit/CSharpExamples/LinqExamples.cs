1356182d64ec5387c43e7339c7de6b9fa814ec82
Michael Cooper <coop87@gmail.com>
2019-02-09 10:49:36 -0500

    Add Why Use F# examples.

---
+++ FSharpForFunAndProfit/CSharpExamples/LinqExamples.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExamples
+{
+    public class LinqExamples
+    {
+        public void Test()
+        {
+            int square(int x) => x * x;
+            int sumOfSquares(int n) => Enumerable.Range(1, n).Select(square).Sum();
+
+            WriteLine(sumOfSquares(100));
+
+            int s2 = square(2);
+            int s3 = square(3);
+            int s4 = square(4);
+
+            ////////////////////////////////////////////////////////////////
+
+            Func<int, int> squareClone = square;
+
+            int execFunction(Func<int, int> aFunc, int aParam) => aFunc(aParam);
+            int result = execFunction(squareClone, 12);
+
+            ////////////////////////////////////////////////////////////////
+
+            int product(int n) => Enumerable.Range(1, n).Aggregate(1, (acc, val) => acc * val);
+            WriteLine(product(10));
+
+            int sumOfOdds(int n) => Enumerable.Range(1, n).Where(x => x % 2 == 1).Sum();
+            WriteLine(sumOfOdds(10));
+
+            int alternatingSum(int n) => Enumerable.Range(1, n)
+                                         .Select((x, i) => i % 2 == 0 ? -x : x)
+                                         .Sum();
+            WriteLine(alternatingSum(10));
+
+            int alternatingSum2(int n) => (Enumerable.Range(1, n)
+                                          .Aggregate(
+                                          (true, 0),
+                                          ((bool neg, int sumSoFar) tuple, int x) => 
+                                          tuple.neg ? (false, tuple.sumSoFar - x) : (true, tuple.sumSoFar + x))).Item2;
+            WriteLine(alternatingSum2(10));
+
+            ////////////////////////////////////////////////////////////////
+            
+
+        }
+    }
+}
---
+++ FSharpForFunAndProfit/Examples/WhyUseFSharp/Scratchpad.fsx
@@ -0,0 +1,111 @@
+﻿
+let square x = x * x
+
+let sumOfSquares n =
+    [1..n] |> List.map square |> List.sum
+
+sumOfSquares 100
+
+let squareClone = square
+let result = [1..10] |> List.map squareClone
+
+let execFunction aFunc aParam = aFunc aParam
+let result2 = execFunction square 12
+
+////////////////////////////////////////////////////////////////
+
+type IntAndBool = { intPart: int; boolPart: bool }
+let x = { intPart = 1; boolPart = false }
+
+type IntOrBool =
+    | IntChoice of int
+    | BoolChoice of bool
+
+let y = IntChoice 42
+let z = BoolChoice true
+
+type Shape =
+    | Circle of radius:int
+    | Rectangle of height:int * width:int
+    | Point of x:int * y:int
+    | Polygon of pointList:(int * int) list
+
+let draw shape =
+    match shape with
+    | Circle radius -> printfn "The cirlce has a radius of %d" radius
+    | Rectangle (height, width) -> printfn "The rectangle is %d high by %d wide" height width
+    | Polygon points -> printfn "The polygon is made of these points %A" points
+    | _ -> printfn "I don't recognize this shape"
+
+let circle = Circle(10)
+let rect = Rectangle(4, 5)
+let point = Point(2, 3)
+let polygon = Polygon( [(1,1); (2,2); (3,3)] )
+
+[circle; rect; polygon; point] |> List.iter draw
+
+////////////////////////////////////////////////////////////////
+
+let i = 1
+let s = "hello"
+let tuple = s,i
+let s2,i2 = tuple
+let list = [s2]
+
+let sumLengths strList =
+    strList |> List.map String.length |> List.sum
+
+////////////////////////////////////////////////////////////////
+
+open System
+
+type Person = { FirstName:string; LastName:string; Dob:DateTime }
+type Coord = { Lat:float; Long:float }
+
+type TimePeriod = Hour | Day | Week | Year
+type Temperature = C of int | F of int
+type Appointment = OneTime of DateTime
+                   | Recurring of DateTime list
+
+////////////////////////////////////////////////////////////////
+
+let product n =
+    let initialValue = 1
+    let action productSoFar x = productSoFar * x
+    [1..n] |> List.fold action initialValue
+
+product 10
+
+let sumOfOdds n =
+    let initialValue = 0
+    let action sumSoFar x = if x % 2 = 0 then sumSoFar else sumSoFar + x
+    [1..n] |> List.fold action initialValue
+
+sumOfOdds 10
+
+let alternatingSum n =
+    let initalValue = (true, 0)
+    let action (isNeg, sumSoFar) x = if isNeg then (false, sumSoFar - x)
+                                     else (true, sumSoFar + x)
+    [1..n] |> List.fold action initalValue |> snd
+
+alternatingSum 100
+
+////////////////////////////////////////////////////////////////
+
+type NameAndSize = { Name : string; Size : int }
+let people = [
+    { Name = "Alice"; Size = 10 }
+    { Name = "Bob"; Size = 1}
+    { Name = "Carol"; Size = 12 }
+    { Name = "David"; Size = 5} ]
+
+people |> List.maxBy (fun item -> item.Size)
+[] |> List.maxBy (fun item -> item.Size)
+
+let maxNameAndSize list =
+    match list with
+    | [] -> None
+    | _ -> Some(list |> List.maxBy (fun item -> item.Size))
+people |> maxNameAndSize
+[] |> maxNameAndSize
