e4325409720e81f505a506e2a6d77ccfe32af27e
Mike Cooper <mikecoop@users.noreply.github.com>
2019-03-02 13:57:58 -0500

    Finish post 4 start 5

---
+++ FSharpForFunAndProfit/Examples/RecursiveTypes/RecursiveTypes4.fsx
@@ -0,0 +1,67 @@
+﻿
+type FileSystemItem =
+    | File of File
+    | Directory of Directory
+and File = { name: string; fileSize: int }
+and Directory = { name: string; dirSize: int; subItems: FileSystemItem list }
+
+let readme = File { name = "readme.txt"; fileSize = 1 }
+let config = File { name = "config.xml"; fileSize = 2 }
+let build  = File { name = "build.bat"; fileSize = 3 }
+let src = Directory { name = "src"; dirSize = 10; subItems = [ readme; config; build ] }
+let bin = Directory { name = "bin"; dirSize = 10; subItems = [ ] }
+let root = Directory { name = "root"; dirSize = 5; subItems = [ src; bin ] }
+
+let rec foldFS fFile fDir acc item : 'r =
+    let recurse = foldFS fFile fDir
+    match item with
+    | File file ->
+        fFile acc file
+    | Directory dir ->
+        let newAcc = fDir acc (dir.name, dir.dirSize)
+        dir.subItems |> List.fold recurse newAcc
+
+let totalSize fileSystemItem =
+    let fFile acc (file:File) =
+        acc + file.fileSize
+    let fDir acc (name, size) =
+        acc + size
+    foldFS fFile fDir 0 fileSystemItem
+
+readme |> totalSize
+src |> totalSize
+root |> totalSize
+
+let largestFile fileSystemItem =
+    let fFile (largestSoFarOpt:File option) (file:File) =
+        match largestSoFarOpt with
+        | None ->
+            Some file
+        | Some largestSoFar ->
+            if largestSoFar.fileSize > file.fileSize then
+                Some largestSoFar
+            else
+                Some file
+    let fDir largestSoFarOpt (name, size) =
+        largestSoFarOpt
+
+    foldFS fFile fDir None fileSystemItem
+
+readme |> largestFile
+src |> largestFile
+bin |> largestFile
+root |> largestFile
+
+let rec firstSumBiggerThan100 sumSoFar listOfInts =
+    match listOfInts with
+    | [ ] ->
+        sumSoFar
+    | head :: tail ->
+        let newSumSoFar = head + sumSoFar
+        if newSumSoFar > 100 then
+            newSumSoFar
+        else
+            firstSumBiggerThan100 newSumSoFar tail
+
+[ 30; 40; 50; 60 ] |> firstSumBiggerThan100 0
+[ 1 .. 3 .. 100 ] |> firstSumBiggerThan100 0
---
+++ FSharpForFunAndProfit/Examples/RecursiveTypes/RecursiveTypes5.fsx
@@ -0,0 +1,86 @@
+﻿
+type LinkedList<'a> =
+    | Empty
+    | Cons of head:'a * tail:LinkedList<'a>
+
+module LinkedList =
+
+    let rec cata fCons fEmpty list : 'r =
+        let recurse = cata fCons fEmpty
+        match list with
+        | Empty ->
+            fEmpty
+        | Cons (element, list) ->
+            fCons element (recurse list)
+
+    let rec foldWithEmpty fCons fEmpty acc list : 'r =
+        let recurse = foldWithEmpty fCons fEmpty
+        match list with
+        | Empty ->
+            fEmpty acc
+        | Cons (element, list) ->
+            let newAcc = fCons acc element
+            recurse newAcc list
+    
+    let rec fold fCons acc list : 'r =
+        let recurse = fold fCons
+        match list with
+        | Empty ->
+            acc
+        | Cons (element, list) ->
+            let newAcc = fCons acc element
+            recurse newAcc list
+
+    let rec foldBack fCons list acc : 'r =
+        let fEmpty' generator =
+            generator acc
+        let fCons' generator element =
+            fun innerResult ->
+                let newResult = fCons element innerResult
+                generator newResult
+        let initialGenerator = id
+        foldWithEmpty fCons' fEmpty' initialGenerator list
+
+    let toList linkedList =
+        let fCons head tail = head :: tail
+        let initialState = [ ]
+        foldBack fCons linkedList initialState
+
+    let ofList list =
+        let fCons head tail = Cons(head, tail)
+        let initialState = Empty
+        List.foldBack fCons list initialState
+
+    let map f list =
+        let folder head tail =
+            Cons (f head, tail)
+
+        foldBack folder list Empty
+
+    let filter pred list =
+        let folder head tail =
+            if pred head then
+                Cons (head, tail)
+            else
+                tail
+        foldBack folder list Empty
+
+    let rev list =
+        let folder tail head =
+            Cons (head, tail)
+        fold folder Empty list
+
+let linkedList = Cons (1, Cons (2, Cons(3, Empty)))
+linkedList |> LinkedList.fold (+) 0
+
+linkedList |> LinkedList.toList
+let list = [ 1; 2; 3 ]
+
+list |> LinkedList.ofList
+
+linkedList |> LinkedList.map (fun i -> i + 10)
+
+let isOdd n = (n % 2 = 1)
+linkedList |> LinkedList.filter isOdd
+
+linkedList |> LinkedList.rev
