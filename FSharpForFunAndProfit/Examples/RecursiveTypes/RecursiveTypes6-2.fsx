74c33bdaa49238b0520096c9601242c6f9cf1fc9
Mike Cooper <mikecoop@users.noreply.github.com>
2019-03-09 16:10:39 -0500

    Recursive Types post 6

---
+++ FSharpForFunAndProfit/Examples/RecursiveTypes/RecursiveTypes6-2.fsx
@@ -0,0 +1,58 @@
+﻿#load "RecursiveTypes6-1.fsx"
+
+open ``RecursiveTypes6-1``
+open System.IO
+
+let foldLinesAsync folder acc (fi:FileInfo) =
+    async {
+        let mutable acc = acc
+        let mutable lineNo = 1
+        use sr = new StreamReader (path = fi.FullName)
+        while not sr.EndOfStream do
+            let! lineText = sr.ReadLineAsync() |> Async.AwaitTask
+            acc <- folder acc lineNo lineText
+            lineNo <- lineNo + 1
+        return acc }
+
+let asyncMap f asyncX = async {
+    let! x = asyncX
+    return (f x) }
+
+let matchPattern textPattern (fi:FileInfo) =
+    let regex = System.Text.RegularExpressions.Regex(pattern = textPattern)
+
+    let folder results lineNo lineText =
+        if regex.IsMatch lineText then
+            let result = sprintf "%40s:%-5i %s" fi.Name lineNo lineText
+            result :: results
+        else
+            results
+
+    fi
+    |> foldLinesAsync folder [ ]
+    |> asyncMap List.rev
+
+let grep filePattern textPattern fileSystemItem =
+    let regex = System.Text.RegularExpressions.Regex(pattern = filePattern)
+
+    let matchFile (fi:FileInfo) =
+        if regex.IsMatch fi.Name then
+            Some (matchPattern textPattern fi)
+        else
+            None
+
+    let fFile asyncs (fi:FileInfo) =
+        (matchFile fi) :: asyncs
+
+    let fDir asyncs (di:DirectoryInfo) =
+        asyncs
+
+    fileSystemItem
+    |> Tree.fold fFile fDir [ ]
+    |> Seq.choose id
+    |> Async.Parallel
+    |> asyncMap (Array.toList >> List.collect id)
+
+currentDir
+|> grep "fsx" "LinkedList"
+|> Async.RunSynchronously
---
+++ FSharpForFunAndProfit/Examples/RecursiveTypes/RecursiveTypes6-3.fsx
@@ -0,0 +1,56 @@
+﻿
+#load "RecursiveTypes6-1.fsx"
+
+open ``RecursiveTypes6-1``
+open System.IO
+
+let nextIdentity =
+    let id = ref 0
+    fun () ->
+        id := !id + 1
+        !id
+
+type PrimaryKey =
+    | FileId of int
+    | DirId of int
+
+let insertDbFile name (fileSize:int64) =
+    let id = nextIdentity()
+    printfn "%10s: inserting id:%i name:%s size:%i" "DbFile" id name fileSize
+    FileId id
+
+let insertDbDir name =
+    let id = nextIdentity()
+    printfn "%10s: inserting id:%i name:%s" "DbDir" id name
+    DirId id
+
+let insertDbDir_File dirId fileId =
+    printfn "%10s: inserting parentDir:%i childFile:%i" "DbDir_File" dirId fileId
+
+let insertDbDir_Dir parentDirId childDirId =
+    printfn "%10s: inserting parentDir:%i childDir:%i" "DbDir_Dir" parentDirId childDirId
+
+let pkToInt primaryKey =
+    match primaryKey with
+    | FileId fileId -> fileId
+    | DirId dirId -> dirId
+
+let insertFileSystemTree fileSystemItem =
+    let fFile (fi:FileInfo) =
+        insertDbFile fi.Name fi.Length
+
+    let fDir (di:DirectoryInfo) childIds =
+        let dirId = insertDbDir di.Name
+        let parentPK = pkToInt dirId
+        childIds |> Seq.iter (fun childId ->
+            match childId with
+            | FileId fileId -> insertDbDir_File parentPK fileId
+            | DirId childDirId -> insertDbDir_Dir parentPK childDirId )
+        dirId
+
+    fileSystemItem
+    |> Tree.cata fFile fDir
+
+let currentDir = fromDirInfo (DirectoryInfo ("."))
+currentDir
+|> insertFileSystemTree
